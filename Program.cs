78d2db4b31f02f3cfd695b1c6f4ba59163d63ce3
William Farmer <willetfarm@yahoo.com>
2020-02-27 10:19:37 +0000

    Add file via upload

---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GradeBook book = new GradeBook();
+
+            IGradeTracker book = CreateGradeBook();
+
+            //GetBookName(book);
+            AddGrades(book);
+            SaveGrades(book);
+            WriteResults(book);
+        }
+
+        private static IGradeTracker CreateGradeBook()
+        {
+            return new ThrowAwayGradeBook();
+        }
+
+        private static void WriteResults(IGradeTracker book)
+        {
+            GradeStatistics stats = book.ComputeStatistics();
+
+            foreach (float grade in book)
+            {
+                Console.WriteLine(grade);
+            }
+
+            //Console.WriteLine(book.Name);
+            WriteResult("Average", stats.AverageGrade);
+            //WriteResult("Highest", (int)stats.HighestGrade);
+            WriteResult("Highest", stats.HighestGrade);
+            WriteResult("Lowest", stats.LowestGrade);
+            WriteResult(stats.Description, stats.LetterGrade);
+            //Console.WriteLine(stats.AverageGrade);
+            //Console.WriteLine(stats.HighestGrade);
+            //Console.WriteLine(stats.LowestGrade);
+        }
+
+        private static void SaveGrades(IGradeTracker book)
+        {
+            using (StreamWriter outputFile = File.CreateText("grades.txt")) // deal with unmanaged code
+            {
+                book.WriteGrades(outputFile);
+                //outputFile.Close();
+            }
+        }
+
+        private static void AddGrades(IGradeTracker book)
+        {
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+        }
+
+        private static void GetBookName(IGradeTracker book)
+        {
+            try
+            {
+                //book.Name = null;
+
+                //book.NameChanged += OnNameChanged;
+
+                //book.Name = "William's Grade Book";
+                //book.Name = "Grade book";
+                Console.WriteLine("Enter a name");
+                book.Name = Console.ReadLine();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                //throw;
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Something went wrong!");
+            }
+        }
+
+        //static void WriteResult(string description, int result)
+        //{
+        //    Console.WriteLine(description + ": " + result);
+        //}
+
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+
+        static void WriteResult(string description, string result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+
+        // Exercise: write test to check app returns correct letter grade
+
+        //static void OnNameChanged(object sender, NameChangedEventArgs args)
+        //{
+        //    Console.WriteLine("Grade book changing name from: '" + args.ExistingName + "' to: '" + args.NewName + "'.");
+        //}
+    }
+}
