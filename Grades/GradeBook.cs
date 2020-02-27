ae7fa45660e19e38c5c8812825d2d4ba3e6178f5
William Farmer <willetfarm@yahoo.com>
2020-02-27 10:21:23 +0000

    Add file via upload

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    public class GradeBook : GradeTracker
+    {
+        public GradeBook()
+        {
+            _name = "Empty";
+            grades = new List<float>();
+        }
+
+        public override GradeStatistics ComputeStatistics()
+        {
+            Console.WriteLine("GradeBook::ComputeStatistics");
+
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+            foreach (float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+            stats.AverageGrade = sum / grades.Count;
+            return stats;
+        }
+        public override void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        public override void WriteGrades(TextWriter destination)
+        {
+            // throw new NotImplementedException();
+            for (int i = grades.Count; i > 0; i--)
+            {
+                destination.WriteLine(grades[i - 1]);
+            }
+        }
+
+        public override IEnumerator GetEnumerator()
+        {
+            return grades.GetEnumerator();
+            //throw new NotImplementedException();
+        }
+
+        protected List<float> grades;
+    }
+}
---
+++ Grades/IGradeTracker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Grades
+{
+    internal interface IGradeTracker : IEnumerable
+    {
+        void AddGrade(float grade);
+        GradeStatistics ComputeStatistics();
+        void WriteGrades(TextWriter destination);
+        string Name { get; set; }
+    }
+}
